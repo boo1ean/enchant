741a33fbf317e52114fea624bb947a43c470dbce
boo1ean <boo1ean0807@gmail.com>
2015-03-20 12:24:10 +0200

    Wire up property removal transform with test

--- tests/index.js
+++ tests/index.js
@@ -26,3 +26,17 @@ test('transform def', function (t) {
 	t.equal(tr().def('abc').apply('bcd'), 'bcd');
 	t.end();
 })
+
+test('transform del', function (t) {
+	var e = tr({
+		a: tr().del(),
+		b: tr().lowercase()
+	});
+
+	t.deepEqual(
+		e({ a: 'beep', b: 'BOOP' }),
+		{ b: 'boop' }
+	);
+
+	t.end();
+});
